8f48f79888a183031fd814ed92d588d891140011
Martyna Chojnowska <martynachojnowska12@gmail.com>
2024-01-10 10:45:54 +0100

    Models migrated

--- evendy_project/evendy/models.py
+++ evendy_project/evendy/models.py
@@ -10,13 +10,12 @@ class Event(models.Model):
     time = models.TimeField()
     place = models.CharField(max_length=250)
     image = models.ImageField(default='event_default.jpg', upload_to='event_pics')
-    # attends_looking_for_company = models.ManyToManyField(Profile, through='AttendsLookingForCompany')
+    # attends_looking_for_company = models.ManyToManyField()
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     date_of_birth = models.DateField()
-    description = models.TextField(blank=True, null=True)
     profile_image = models.ImageField(default='profile_default.jpg', upload_to='profile_pics')
     user_planned_events = models.ManyToManyField(Event, through='UserPlannedEvent')
     #user_friends_from_events =
