9edb0055759fb92192de53b7a5c9e4e1bf0bad35
Martyna Chojnowska <martynachojnowska12@gmail.com>
2024-01-11 13:17:27 +0100

    Added home view

--- evendy_project/evendy/urls.py
+++ evendy_project/evendy/urls.py
@@ -4,6 +4,7 @@ from django.contrib.auth import views
 
 
 urlpatterns = [
+    path('', evendy_views.EventListView.as_view(), name='events_list'),
     path('register/', evendy_views.register, name='register'),
     path('login/', views.LoginView.as_view(template_name='evendy/login.html'), name='login'),
     path('logout/', views.LogoutView.as_view(template_name='evendy/logout.html'), name='logout'),
