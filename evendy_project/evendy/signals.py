6933577ef878bf294232c1e6efbdf3d56b2c992f
Martyna Chojnowska <martynachojnowska12@gmail.com>
2024-01-23 00:05:38 +0100

    All updated

--- evendy_project/evendy/signals.py
+++ evendy_project/evendy/signals.py
@@ -1,10 +1,7 @@
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
-from .models import Profile, Invitation
-import datetime
-from notifications.signals import notify
-
+from .models import Profile
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
