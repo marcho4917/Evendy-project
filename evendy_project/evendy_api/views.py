e38e31f6914179135175242ef4c0fd26da05fa22
Martyna Chojnowska <martynachojnowska12@gmail.com>
2024-02-13 22:50:56 +0100

    Updated latest funcionality

--- evendy_project/evendy_api/views.py
+++ evendy_project/evendy_api/views.py
@@ -11,34 +11,36 @@ def events_list(request):
     events_data = response.json()
 
     for event_data in events_data['_embedded']['events']:
-        title = event_data['name']
-        date = event_data['dates']['start']['localDate']
-        time = event_data['dates']['start']['localTime']
+        if event_data['dates']['status']['code'] != 'cancelled':
+            title = event_data['name']
+            date = event_data['dates']['start']['localDate']
+            time = event_data['dates']['start']['localTime']
 
-        for venue_data in event_data['_embedded']['venues']:
-            place = venue_data['city']['name']
+            for venue_data in event_data['_embedded']['venues']:
+                place = venue_data['city']['name']
 
-        images = event_data['images']
+            images = event_data['images']
 
-        for image in images:
-            if image.get('ratio') == '16_9':
-                url_16_9 = image.get('url')
+            for image in images:
+                if image.get('ratio') == '16_9':
+                    url_16_9 = image.get('url')
+                    break
 
             event_instance, created = Event.objects.get_or_create(
-                defaults={},
-                #ma sprawdzac  po konkretnych wartosciah np jesli jest juz title place i  time to nie tworzy tylko zrob get
                 title=title,
                 date=date,
                 time=time,
-                place=place,
-                image=url_16_9
-            )
+                defaults={
+                    'place': place,
+                    'image': url_16_9
+                    }
+                )
 
-    events = Event.objects.all()
+        events = Event.objects.all()
 
-    paginator = Paginator(events, 8)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
+        paginator = Paginator(events, 8)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
     return render(request, 'evendy_api/evendy_api.html', {'page_obj': page_obj})
 
 
