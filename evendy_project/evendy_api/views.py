11e198e80e774067cb87318b3afcd9161f0bbfa4
Martyna Chojnowska <martynachojnowska12@gmail.com>
2024-02-14 09:11:09 +0100

    All events loaded

--- evendy_project/evendy_api/views.py
+++ evendy_project/evendy_api/views.py
@@ -1,6 +1,5 @@
 import requests
 from django.core.paginator import Paginator
-from django.http import JsonResponse
 from django.shortcuts import render
 from evendy.models import Event
 
@@ -10,37 +9,46 @@ def events_list(request):
     response = requests.get(url)
     events_data = response.json()
 
-    for event_data in events_data['_embedded']['events']:
-        if event_data['dates']['status']['code'] != 'cancelled':
-            title = event_data['name']
-            date = event_data['dates']['start']['localDate']
-            time = event_data['dates']['start']['localTime']
+    all_events = []
 
-            for venue_data in event_data['_embedded']['venues']:
-                place = venue_data['city']['name']
+    total_pages = events_data['page']['totalPages']
 
-            images = event_data['images']
+    for page_number in range(total_pages):
+        response = requests.get(url, {'page': page_number})
+        events_data = response.json()
 
-            for image in images:
-                if image.get('ratio') == '16_9':
-                    url_16_9 = image.get('url')
-                    break
+        for event_data in events_data['_embedded']['events']:
+            if event_data['dates']['status']['code'] != 'cancelled':
+                title = event_data['name']
+                date = event_data['dates']['start']['localDate']
+                time = event_data['dates']['start']['localTime']
 
-            event_instance, created = Event.objects.get_or_create(
-                title=title,
-                date=date,
-                time=time,
-                defaults={
-                    'place': place,
-                    'image': url_16_9
-                    }
-                )
+                for venue_data in event_data['_embedded']['venues']:
+                    place = venue_data['city']['name']
 
-        events = Event.objects.all()
+                images = event_data['images']
 
-        paginator = Paginator(events, 8)
-        page_number = request.GET.get('page')
-        page_obj = paginator.get_page(page_number)
+                for image in images:
+                    if image.get('ratio') == '16_9':
+                        url_16_9 = image.get('url')
+                        break
+
+                event_instance, created = Event.objects.get_or_create(
+                    title=title,
+                    date=date,
+                    time=time,
+                    defaults={
+                        'place': place,
+                        'image': url_16_9
+                        }
+                    )
+
+                all_events.append(event_instance)
+
+    paginator = Paginator(all_events, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     return render(request, 'evendy_api/evendy_api.html', {'page_obj': page_obj})
 
 
