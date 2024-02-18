01af68a8f1960989e294ed251886b9a0e91867aa
Martyna Chojnowska <martynachojnowska12@gmail.com>
2024-02-19 00:58:42 +0100

    User can now update his age, and phone number

--- evendy_project/evendy/forms.py
+++ evendy_project/evendy/forms.py
@@ -1,30 +1,30 @@
 from django.contrib.auth.forms import UserCreationForm, User
 from django import forms
 from .models import Profile
+import phonenumbers
 
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
-    phone_number = forms.CharField(max_length=12)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2', 'phone_number']
+        fields = ['username', 'email', 'password1', 'password2']
 
 
 class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField()
-    date_of_birth = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}), required=False)
-    phone_number = forms.CharField(max_length=12)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'date_of_birth', 'phone_number']
+        fields = ['username', 'email']
 
 
 class ProfileUpdateForm(forms.ModelForm):
     description = forms.TextInput()
+    phone_number = forms.CharField(max_length=12, help_text='Correct phone format: 0xxxxxxxxxx')
+    date_of_birth = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}), required=False)
 
     class Meta:
         model = Profile
-        fields = ['profile_image', 'description']
+        fields = ['profile_image', 'description', 'phone_number', 'date_of_birth']
