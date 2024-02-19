27c422fdedc755e91348df1bca32c8972a86b978
Martyna Chojnowska <martynachojnowska12@gmail.com>
2024-02-19 01:00:06 +0100

    Added message tags for error message(red color available now)

--- evendy_project/evendy_project/settings.py
+++ evendy_project/evendy_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -140,3 +141,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'events_list'
 
 LOGIN_URL = 'login'
+
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
