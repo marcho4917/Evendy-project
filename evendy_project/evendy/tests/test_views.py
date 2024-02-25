4c6621642a24a1ab9cd1888ea5ba5e6b3a1005c7
Martyna Chojnowska <martynachojnowska12@gmail.com>
2024-02-25 23:17:33 +0100

    Tests for evendy_app views corrected

--- evendy_project/evendy/tests/test_views.py
+++ evendy_project/evendy/tests/test_views.py
@@ -1,4 +1,5 @@
 import pytest
+from django.urls import reverse
 from evendy.models import Event, UserPlannedEvent, Profile
 
 
@@ -12,34 +13,47 @@ def test_if_register_form_is_render(client):
 
 
 def test_if_profile_view_is_render_with_authenticated_client(client, django_user_model):
-    username = "test-user"
-    password = "test-password"
-    user = django_user_model.objects.create_user(username=username, password=password)
-    client.login(username=username, password=password)
+    user = django_user_model.objects.create_user(username='test-user', password='test-password')
+    client.force_login(user)
     response = client.get('/profile/')
 
     assert response.status_code == 200
 
 
 def test_if_event_details_view_is_render(client):
-    event = Event.objects.create(title='test-event', date='2024-06-06')
+    Event.objects.create(title='test-event', date='2024-06-06')
     response = client.get('/event-details/1/')
 
     assert response.status_code == 200
 
 
 def test_if_user_planned_events_are_correctly_added_to_context(client, django_user_model):
-    username = "test-user"
-    password = "test-password"
-    user = django_user_model.objects.create_user(username=username, password=password)
-    profile = Profile.objects.get(user=user)
-    client.login(username=username, password=password)
+    user = django_user_model.objects.create_user(username='test-user', password='test-password')
+    client.force_login(user)
 
     event = Event.objects.create(title='test-event', date='2024-06-06')
-    UserPlannedEvent.objects.create(profile=profile, event=event)
+    UserPlannedEvent.objects.create(profile=user.profile, event=event)
+
+    url = reverse('add_or_remove_user_from_seekers', args=[event.id])
 
     response = client.get('/my-events/')
+    response_with_action = client.post(url, {'action': 'add'})
 
+    assert UserPlannedEvent.objects.filter(profile=user.profile, event=event).exists()
     assert response.status_code == 200
 
 
+def test_if_user_planned_events_are_correctly_removed_from_context(client, django_user_model):
+    user = django_user_model.objects.create_user(username='test-user', password='test-password')
+    client.force_login(user)
+
+    event = Event.objects.create(title='test-event', date='2024-06-06')
+    UserPlannedEvent.objects.create(profile=user.profile, event=event)
+
+    url = reverse('add_or_remove_user_from_seekers', args=[event.id])
+
+    response = client.get('/my-events/')
+    response_with_action = client.post(url, {'action': 'remove'})
+
+    assert not UserPlannedEvent.objects.filter(profile=user.profile, event=event).exists()
+    assert response.status_code == 200
